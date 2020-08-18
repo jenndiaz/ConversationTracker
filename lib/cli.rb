ffbcc86a10e07f2ba920f772cd176b30478abe40
github username <github email address>
2020-08-18 16:32:55 -0600

    began main_menu

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,5 @@
 class Cli 
-    Prompt = TTY::Prompt.new
+    prompt = TTY::Prompt.new
     attr_accessor :username_input
 
     def welcome 
@@ -22,6 +22,28 @@ class Cli
     end
 
     def main_menu 
+        menu_options = {
+            "Create New Converastion" => 1,
+            "Delete Conversation" => 2,
+            "Update Conversation" => 3, 
+            "View Your Friends" => 4, 
+            "Exit Friendly Reminder" => 5
+        }
+
+        menu_response = prompt.select("Choose an option from below:")
+        case menu_response
+        when 1
+            
+        when 2
+
+        when 3 
+
+        when 4 
+        
+        when 5 
+            puts "We hope you enjoied your Friendly Reminder! Come back soon!"
+            welcome
+        end
 
     end
 
