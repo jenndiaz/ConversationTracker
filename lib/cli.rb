07ad7d1a8c284f4c5771c512876617f9a22294d8
github username <github email address>
2020-08-20 16:22:28 -0600

    create error debugged

--- lib/cli.rb
+++ lib/cli.rb
@@ -12,7 +12,6 @@ class Cli
         system("clear")
         welcome_art
         prompt = TTY::Prompt.new(active_color: :cyan)
-        # binding.pry
         puts "Welcome to Friendly Reminder"
         puts "The friendly app to track your friends and stay in touch!"
         @username_input = prompt.ask("Please enter a new or existing username:".colorize(:cyan)) do |q|
@@ -28,23 +27,23 @@ class Cli
                 puts "Welcome back, #{@found_user.username}!"
                 sleep(1)
             else
-                @new_user = Account.create(username: @username_input).save
+                @new_user = Account.create(username: @username_input)
                 puts "Welcome new friend, #{@username_input}!"
                 sleep(1)
             end
             main_menu
         end
     end
-    
+
     def main_menu 
         prompt = TTY::Prompt.new(active_color: :cyan)
         choices = {
             "Add New Conversation with Friend" => 1,
             "Delete Conversation with Friend" => 2,
             "Update Conversation with Friend" => 3, 
-            "View Your Friends" => 4, 
-            "Exit Friendly Reminder" => 5,
-            "Test View Friends/Dates" => 6
+            "View Your Friends and Conversation Date" => 4, 
+            "Exit Friendly Reminder" => 5
+    
         }
         menu_response = prompt.select("Choose an option from below:".colorize(:cyan), choices)
         case menu_response
@@ -55,9 +54,15 @@ class Cli
                 q.modify :strip, :capitalize 
                 end
             date = prompt.ask ("On what date was your most recent conversation?")
+           #binding.pry 
             new_friend = Friend.create(name: new_friend_name, occupation: nil)
-            Conversation.create(account: @found_user, friend: new_friend, date: date).save
-            puts "Your new friend has been entered! Remeber to keep in touch!"
+            new_friend.save
+            if @found_user
+                Conversation.create(account: @found_user, friend: new_friend, date: date)
+            elsif @new_user
+                Conversation.create(account: @new_user, friend: new_friend, date: date)
+            end
+            puts "Your new friend has been entered! Remember to keep in touch!"
            # binding.pry
            sleep(1)
            system("clear")
@@ -65,9 +70,11 @@ class Cli
            main_menu
         when 2 #delete
             exfriend = prompt.ask("Who would you like to delete?")
-            exfriend_name = Friend.find_by(name: exfriend)
+            friend_instance = Friend.find_by(name: exfriend)
+            exfriend_name = Conversation.find_by(friend: friend_instance)
                 if exfriend_name == nil
                     puts "You haven't started a conversation with this friend yet. Add a new conversation to get started.".colorize(:red)
+                # binding.pry
                 else
                     exfriend_name.destroy.save
                     puts "Your conversation has been deleted! :( Time to make new friends!"
@@ -77,7 +84,7 @@ class Cli
             welcome_art
             main_menu
         when 3 #Update
-            prompt = TTY::Prompt.new
+            prompt = TTY::Prompt.new(active_color: :cyan)
             friend_chat = prompt.ask("Great job reaching out to a friend! Whom did you speak with?")
             sleep(1)
             friend = Friend.find_by(name: friend_chat)
@@ -96,50 +103,30 @@ class Cli
             main_menu
         when 4 #View friends
             if @found_user
-                yourfriends = @found_user.friends
-            elsif @new_user
-                yourfriends = @new_user.friends
-                end
-            yourfriends.reload
-            if yourfriends.empty?
-                puts "You haven't added any friends yet. Add a new conversation to get started.".colorize(:red)
-            else
-                puts "Here are your friends!"
-                sleep(1)
-                yourfriends.each do |friend|
-                puts friend.name
-                end
-            end
-            sleep(1.5)
-            system("clear")
-            welcome_art
-            main_menu
-        when 5 
-            puts "We hope you enjoyed your Friendly Reminder! Come back soon!"
-            sleep(2)
-            exit
-        when 6 #test
-            if @found_user
                 found_user_id = @found_user.id
                 yourconversations = Conversation.where(account: found_user_id)
             elsif @new_user
-                new_user_id = @new_user.id
+                new_user_id = @new_user
                 yourconversations = Conversation.where(account: new_user_id)
-                end
+            end
+                 yourconversations.reload           
             if yourconversations.empty?
                 puts "You haven't added any friends yet. Create a new conversation to get started.".colorize(:red)
             else
                 puts "Here are your friends!"
                 sleep(1)
                 yourconversations.each do |conversation|
-                    puts "#{conversation.date} + #{conversation.friend.name}"
+                    puts "#{conversation.friend.name}:  #{conversation.date} "
                 end
             end
-        end
             sleep(1.5)
-            main_menu
+            main_menu      
+        when 5 
+            puts "We hope you enjoyed your Friendly Reminder! Come back soon!"
+            sleep(2)
+            exit
         end
-
+    end        
         # def view_conversations
         #     if @found_user
         #         yourconversations = Conversation.where(account_id: @found_user.account_id)
@@ -159,3 +146,22 @@ class Cli
         #     main_menu
         # end
     # end
+    # if @found_user
+    #     yourfriends = @found_user.friends
+    # elsif @new_user
+    #     yourfriends = @new_user.friends
+    #     end
+    # yourfriends.reload
+    # if yourfriends.empty?
+    #     puts "You haven't added any friends yet. Add a new conversation to get started.".colorize(:red)
+    # else
+    #     puts "Here are your friends!"
+    #     sleep(1)
+    #     yourfriends.each do |friend|
+    #     puts friend.name
+    #     end
+    # end
+    # sleep(1.5)
+    # system("clear")
+    # welcome_art
+    # main_menu
